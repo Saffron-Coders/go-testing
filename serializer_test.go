a97f8a513048de1e44769dfa149acc5dadc1c87a
Dave Augustus <davetweetlive@gmail.com>
2021-07-17 00:59:07 +0530

    testify package

--- serializer_test.go
+++ serializer_test.go
@@ -1,12 +1,12 @@
 package serializer_test
 
 import (
-	"errors"
 	"strconv"
 	"testing"
 	"time"
 
 	jsonserializer "github.com/davetweetlive/jsonserializer"
+	"github.com/stretchr/testify/require"
 )
 
 var fileName = "tmp/user.json"
@@ -14,53 +14,31 @@ var fileName = "tmp/user.json"
 func TestCreateUsers(t *testing.T) {
 	t.Parallel()
 	users, err := jsonserializer.CreateUsers()
-	if err != nil {
-		t.Errorf("error shoube be nil, but got %v", err)
-	}
-
-	if len(users) != 3 {
-		t.Errorf("users length should be 3, but got %v", len(users))
-	}
-
-	if users[1].FirstName != "Irak" {
-		t.Errorf("the firstname should be Irak but got %v", users[1].FirstName)
-	}
+	require.NoError(t, err)
+	require.Equal(t, len(users), 3)
+	require.Equal(t, users[1].FirstName, "Irak")
 }
 
 func TestWriteStructToJSONFile(t *testing.T) {
 	t.Parallel()
 	users, err := jsonserializer.CreateUsers()
-	if err != nil {
-		t.Errorf("error shoube be nil, but got %v", err)
-	}
+	require.NoError(t, err)
 
 	err = jsonserializer.WriteStructToJSONFile(users, fileName)
-	if err != nil {
-		t.Errorf("Couldn't write to JSON file")
-	}
+	require.NoError(t, err)
 }
 
 func TestFromJSONFileToStruct(t *testing.T) {
 	t.Parallel()
 	users, err := jsonserializer.FromJSONFileToStruct(fileName)
-	if err != nil {
-		t.Errorf("error shoube be nil, but got %v", err)
-	}
-	if len(users) != 3 {
-		t.Errorf("users length should be 3, but got %v", len(users))
-	}
-
-	if users[1].FirstName != "Irak" {
-		t.Errorf("the firstname should be Irak but got %v", users[1].FirstName)
-	}
+	require.NoError(t, err)
+	require.Equal(t, len(users), 3)
+	require.Equal(t, users[2].FirstName, "Imaginery")
 }
 
 func TestCreateUsersSubTest(t *testing.T) {
 	users, err := jsonserializer.CreateUsers()
-	if err != nil {
-		t.Errorf("error shoube be nil, but got %v", err)
-	}
-
+	require.NoError(t, err)
 	batch := []struct {
 		batch int
 		users []*jsonserializer.User
@@ -95,30 +73,20 @@ func TestCreateUsersSubTest(t *testing.T) {
 		},
 	}
 
-	if batch == nil {
-		t.Errorf("Errro %v", errors.New("batch shouldn't be empty"))
-	}
+	require.NotEqual(t, batch, nil)
 
 	for i, tc := range batch {
 		t.Run(strconv.Itoa(tc.batch), func(t *testing.T) {
 			t.Parallel()
 			if tc.batch < 1 || tc.batch > 3 {
-				t.Errorf("Batch can't be more then three or less than equal to zeor, got %v", tc.batch)
-				if tc.users[i].FirstName == "" {
-					t.Errorf("firstname can't be empty")
-				}
-				if tc.users[i].LastName == "" {
-					t.Errorf("lastname can't be empty")
-				}
-				if tc.users[i].Email == "" {
-					t.Errorf("email can't be empty")
-				}
-			}
 
-			if len(tc.users) == 0 {
-				t.Error("user's list shouldn't be zero, got ", len(tc.users))
-			}
+				require.NotEqual(t, tc.users[i].FirstName, "")
 
+				require.NotEqual(t, tc.users[i].LastName, "")
+
+				require.NotEqual(t, tc.users[i].Email, "")
+			}
+			require.NotEqual(t, len(tc.users), 0)
 		})
 	}
 }
